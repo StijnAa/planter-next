7ca60ce256fcc8205dbc104dd26264541aa1f6e0
StijnAa <s.tijnaa@gmail.com>
2022-12-20 22:21:12 +0100

    lost of shit

--- components/shoppingcard/shoppingItem.js
+++ components/shoppingcard/shoppingItem.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import theme from "../../styles/theme";
-import Link from "../link/link.component";
 import Image from "next/image";
 
 const ShoppingItemContainer = styled.li`
